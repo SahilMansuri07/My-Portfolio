4dc4e7e141d6a75f5e56cc767ef236d80022d4ae
sahil mansuri <sahilmansuri881.o@gmail.com>
2025-06-15 20:39:01 +0530

    change avatar

--- src/Sections/home.jsx
+++ src/Sections/home.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <section className="w-full min-h-screen text-white flex flex-col items-center justify-center px-4 pt-24 pb-16">
       {/* Profile Picture */}
       <img
-        src="https://avatars.githubusercontent.com/u/102119202?v=4"
+        src="../../public/avatar.png"
         alt="Sahil Mansuri"
         className="h-32 w-32 rounded-full border-4 border-white shadow-lg mb-6"
       />
@@ -27,19 +27,24 @@ export default function Hero() {
         something new. Let’s build something great together!
       </p>
 
-      {/* Buttons */}
-      <div className="flex flex-col sm:flex-row gap-4 mt-6 w-full max-w-xs sm:max-w-none sm:justify-center">
-        <button className="w-full sm:w-auto bg-gradient-to-r from-pink-500 to-indigo-500 text-white px-6 py-2 rounded-full font-medium hover:opacity-90 transition">
-          Show CV
-        </button>
-        <a
-          href="/cv.pdf"
-          download
-          className="w-full sm:w-auto text-center bg-white text-purple-700 px-6 py-2 rounded-full font-semibold hover:bg-gray-200 transition"
-        >
-          Download CV
-        </a>
-      </div>
+    <div className="flex flex-col sm:flex-row gap-4 mt-6 w-full max-w-xs sm:max-w-none sm:justify-center">
+  <a
+    href="././public/SahilMansuriResume.pdf"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="w-full sm:w-auto bg-gradient-to-r from-pink-500 to-indigo-500 text-white px-6 py-2 rounded-full font-medium hover:opacity-90 transition text-center"
+  >
+    Show CV
+  </a>
+  <a
+    href="././public/SahilMansuriResume.pdf"
+    download
+    className="w-full sm:w-auto text-center bg-white text-purple-700 px-6 py-2 rounded-full font-semibold hover:bg-gray-200 transition"
+  >
+    Download CV
+  </a>
+</div>
+
     </section>
   );
 }
